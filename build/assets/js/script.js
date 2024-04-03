da523555f4b845512897f9c825c3ee58a7a57632
Emmanuel-Anaba <emmaanaba29@gmail.com>
2024-04-03 23:47:20 +0100

    done ✔

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,6 +1,6 @@
 const ul = document.querySelector("ul");
 const input = document.getElementById("taskInput");
-let itemsArray = JSON.parse(localStorage.getItem("items")) || [];
+const itemsArray = JSON.parse(localStorage.getItem("items")) || [];
 
 itemsArray.forEach(addTask);
 
@@ -8,35 +8,36 @@ function addTask(text) {
   const li = document.createElement("li");
   const div = document.createElement("div");
   const p = document.createElement("p");
-  const textNode = document.createTextNode(text);
   const deleteBtn = document.createElement("button");
   const checkBox = document.createElement("input");
+
   checkBox.type = "checkbox";
   deleteBtn.textContent = "Delete";
 
-  deleteBtn.addEventListener("click", function () {
-    const taskText =
-      this.parentElement.parentElement.querySelector("p").textContent;
-    const index = itemsArray.indexOf(taskText);
-    if (index !== -1) {
-      itemsArray.splice(index, 1);
-      localStorage.setItem("items", JSON.stringify(itemsArray));
-      this.parentElement.parentElement.remove();
-    }
-  });
+  deleteBtn.addEventListener("click", deleteTask);
+  checkBox.addEventListener("change", toggleTask);
 
-  checkBox.addEventListener("change", function () {
-    const p = this.parentElement.querySelector("p");
-    p.style.textDecoration = this.checked ? "line-through" : "none";
-    p.classList.toggle("text-emerald-500", this.checked);
-  });
-
-  p.appendChild(textNode);
-  div.appendChild(deleteBtn);
-  div.appendChild(checkBox);
-  li.appendChild(p);
-  li.appendChild(div);
+  p.appendChild(document.createTextNode(text));
+  div.append(deleteBtn, checkBox);
+  li.append(p, div);
   ul.appendChild(li);
+}
+
+function deleteTask() {
+  const taskText =
+    this.parentElement.parentElement.querySelector("p").textContent;
+  const index = itemsArray.indexOf(taskText);
+  if (index !== -1) {
+    itemsArray.splice(index, 1);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    this.parentElement.parentElement.remove();
+  }
+}
+
+function toggleTask() {
+  const p = this.parentElement.parentElement.querySelector("p");
+  p.style.textDecoration = this.checked ? "line-through" : "none";
+  p.classList.toggle("text-emerald-500");
 }
 
 function add() {
